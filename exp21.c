448592d0e16d42632937d02825f8d6916803e6e1
Sumalatha2007 <192424023.simats@saveetha.com>
2026-08-13 09:43:27 +0530

    Add files via upload

---
+++ exp21.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char sentence[200];
+    int vowels = 0;
+    int i;
+
+    printf("Enter a sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    for (i = 0; sentence[i] != '\0'; i++)
+    {
+        char ch = tolower(sentence[i]);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' ||
+            ch == 'o' || ch == 'u')
+        {
+            vowels++;
+        }
+    }
+
+    printf("Number of vowels = %d\n", vowels);
+
+    return 0;
+}
